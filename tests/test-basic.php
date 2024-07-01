1f7b2c531c50fa1cde4b1eed64d41987bf154ce3
fumikito <guy@hametuha.com>
2024-07-02 03:05:02 +0900

    Add debug class.

--- tests/test-basic.php
+++ tests/test-basic.php
@@ -21,29 +21,14 @@ class Taf_Basic_Test extends WP_UnitTestCase {
 	}
 
 	function test_position() {
-		// Create Terms
-		add_filter( 'taf_default_positions', function() {
-			return [
-				'after_title' => [
-					'name'        => 'After Title',
-					'description' => 'This is a position.',
-					'mode'        => 'iframe',
-				],
-			];
-		} );
 		$result = taf_register_positions();
-		$this->assertEquals( 1, $result );
+		$this->assertEquals( 4, $result );
 		// Check existence
-		$term = get_term_by( 'slug', 'after_title', 'ad-position' );
+		$term = get_term_by( 'slug', 'iframe', 'ad-position' );
 		$this->assertTrue( $term || ! is_wp_error( $term ) );
 		$this->assertEquals( 'iframe', get_term_meta( $term->term_id, 'taf_display_mode', true ) );
 
 		// Check clear method.
 		$this->assertTrue( taf_clear_terms() );
-
-
 	}
-
-
-
 }
