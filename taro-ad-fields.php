c3e817b7ef8f0331aa6015d7f97f8f569a8e4e8a
fumikito <guy@hametuha.com>
2024-07-01 22:14:42 +0900

    travisを削除

--- taro-ad-fields.php
+++ taro-ad-fields.php
@@ -2,14 +2,13 @@
 /*
 Plugin Name: Taro Ad Fields
 Plugin URI: https://wordpress.org/plugin/taro-ad-fields
-Description: Add ad block for advertisments
+Description: Add ad blocks for advertisement.
 Author: Tarosky INC.
 Author URI: https://tarosky.co.jp
 Text Domain: taf
 Domain Path: /languages/
 License: GPL v3 or later.
-Version: 1.1.0
-PHP Version: 5.4.0
+Version: nightly
 */
 
 add_action( 'plugins_loaded', 'taro_ad_field_init' );
