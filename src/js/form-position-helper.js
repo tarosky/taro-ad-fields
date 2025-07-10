36b565ad8aec41052c2fb7d8f53cf3ccbe45f5c0
fumikito <guy@hametuha.com>
2025-07-10 14:54:30 +0900

    #45 iframeのエスケープがJSだと簡単ではなかったので、Reactを使う

--- src/js/form-position-helper.js
+++ src/js/form-position-helper.js
@@ -2,7 +2,7 @@
  * Form helper in position edit screen.
  *
  * @handle taf-form-position-helper
- * @deps wp-i18n
+ * @deps wp-i18n, wp-element
  */
 
 /* global TafIframeData:false */
@@ -17,45 +17,50 @@
 const data = TafIframeData;
 
 const { __ } = wp.i18n;
+const { createRoot, render } = wp.element;
 
-document.addEventListener( 'DOMContentLoaded', function() {
+/**
+ * Creates a container for the iframe URL and example usage.
+ *
+ * @param displayMode
+ * @returns {JSX.Element}
+ */
+const TafIframeContainer = ( { displayMode } ) => {
+	return (
+		<div className={ displayMode ? 'taf-iframe-container' : '' } style={ { marginTop: '40px' } }>
+			<hr />
+			<h2>{ __( 'Iframe URL', 'taf' ) }</h2>
+			<p className="description" style={ { marginTop: '1em' } }>
+				{ displayMode ? __(
+					'This is the iframe URL along with an example of how to use it. You can adjust the attributes (such as size or styling) to fit your specific use case.',
+					'taf'
+				) : __(
+					'The iframe URL will appear here when the Display Mode is set to "iframe". If the Display Mode is set to "Default", this message will be shown instead.',
+					'taf'
+				) }
+			</p>
+			{ displayMode && (
+				<>
+					<div style={ { margin: '1em 0' } }>
+						<input type="url" style={ { width: '100%', boxSizing: 'border-box' } } readOnly={ true } value={ data.iframeUrl } onFocus={ (event) => event.target.select() } />
+					</div>
+					<div style={ { margin: '1em 0' } }>
+						<input type="url"  style={ { width: '100%', boxSizing: 'border-box' } } readOnly={ true } value={ data.iframeElement } onFocus={ (event) => event.target.select() } />
+					</div>
+				</>
+			) }
+		</div>
+	);
+};
+
+// Render the iframe container when the DOM is ready.
+document.addEventListener( 'DOMContentLoaded', function () {
 	const container = document.createElement( 'div' );
-	container.style.marginTop = '40px';
-	if ( data.displayMode ) {
-		container.classList.add( 'taf-iframe-container' );
-
-		container.innerHTML = `
-			<hr>
-			<h2>${ __( 'Iframe URL', 'taf' ) }</h2>
-			<p style="margin: 1em 0;" class="description">${ __(
-		'This is the iframe URL along with an example of how to use it. You can adjust the attributes (such as size or styling) to fit your specific use case.',
-		'taf'
-	) }</p>
-			<div style="margin: 1em 0;">
-				<input type="url" style="width: 100%; box-sizing: border-box" readonly="" value="${
-	data.iframeUrl
-}" onfocus="this.select()">
-			</div>
-			<div style="margin: 1em 0;">
-				<input type="url" style="width: 100%; box-sizing: border-box" readonly="" value="${
-	data.iframeElement
-}" onfocus="this.select()">
-			</div>
-		`;
-	} else {
-		container.innerHTML = `
-			<hr>
-			<h2><?php esc_html_e( 'Iframe URL', 'taf' ); ?></h2>
-			<p style="margin: 1em 0;" class="description">${ __(
-		'The iframe URL will appear here when the Display Mode is set to "iframe". If the Display Mode is set to "Default", this message will be shown instead.',
-		'taf'
-	) }</p>
-		`;
-	}
-
 	// Add below the form
 	const form = document.querySelector( 'form#edittag' );
 	if ( form && form.parentNode ) {
 		form.parentNode.appendChild( container );
+		const root = createRoot( container );
+		root.render( <TafIframeContainer displayMode={ data.displayMode }  /> );
 	}
 } );
